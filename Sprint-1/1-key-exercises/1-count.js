9e8021ae345e7443e60e12a574fb2f777789214d
aliciawt <aliciawynonatjahjadi@gmail.com>
2025-11-29 13:33:42 +0100

    Answer to Key Exercise 1

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,5 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// Line 3 is reassigning the value for variable count by adding 1 to variable count, so the variable count will evaluate to 1.
+// The single equal sign (=) is an assignment operator. It assigns the value on the right side to the variable on the left side.
