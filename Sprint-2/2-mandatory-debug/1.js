8bacf811d89f6263dd2188f16017e5a9e425780f
aliciawt <aliciawynonatjahjadi@gmail.com>
2025-12-15 21:37:39 +0100

    Answer Mandatory Debug 1

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,7 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// Return will return nothing since no value is assigned to it.
+// The line a + b never runs because a function ends with 'return'.
 
 function sum(a, b) {
   return;
@@ -9,5 +11,11 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+// The console log will print 'undefined' on the call function.
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
