719e1250d2f25c2401a4bc03a71a4f2db27bd163
aliciawt <aliciawynonatjahjadi@gmail.com>
2025-12-15 20:01:58 +0100

    Answer Key Errors 1

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,6 +2,7 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
+// It will throw an error because decimalNumber is the input for the function, but below we are trying to declare it again using const.
 
 // Try playing computer with the example to work out what is going on
 
@@ -15,6 +16,12 @@ function convertToPercentage(decimalNumber) {
 console.log(decimalNumber);
 
 // =============> write your explanation here
+// This function would not run because the variable decimalNumber is declared twice.
+// In order for it to work, another variable with a different name must be created.
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function convertToPercentage (decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+  return percentage;
+}
