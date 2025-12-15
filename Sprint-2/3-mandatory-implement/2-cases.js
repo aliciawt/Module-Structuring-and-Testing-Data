352ac6a0c3221aca94da41794e2580cec1a6825b
aliciawt <aliciawynonatjahjadi@gmail.com>
2025-12-15 22:11:54 +0100

    Answer Mandaory Implement 2

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,7 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+function toUpperSnakeCase(string) {
+    return string.split(" ").join("_").toUpperCase();
+}
