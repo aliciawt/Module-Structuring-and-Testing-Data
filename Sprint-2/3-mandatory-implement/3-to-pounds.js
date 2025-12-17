9a4e368e2687cd80c101771e85670f62ac52daba
Alicia Wynona Tjahjadi <alicia@intranet.rosadelsvents.es>
2025-12-17 10:32:58 +0100

    Answer Mandatory Implement 3

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,23 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+function toPounds(penceString) {
+    // remove the "p"
+    const penceStringWithoutP = penceString.replace("p", "");
+
+    // separate pounds and pence
+    let pence = penceStringWithoutP.slice(-2).padStart(2, "0");
+    let pounds = penceStringWithoutP.slice(0, -2).padStart(1, "0");
+
+    // convert to number and format with commas
+    pounds = Number(pounds).toLocaleString('en-GB'); 
+
+    return `£${pounds}.${pence}`;
+}
+
+// examples
+console.log(toPounds("5p"));        // £0.05
+console.log(toPounds("99p"));       // £0.99
+console.log(toPounds("1234p"));     // £12.34
+console.log(toPounds("1234567p"));  // £12,345.67
