0e19315efdd1fe5d284268a22f0c8500c7e2a9ee
aliciawt <aliciawynonatjahjadi@gmail.com>
2026-01-07 22:09:09 +0100

    Answer Mandatory Rewrite 3

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,10 @@
 function getCardValue(card) {
-    // replace with your code from key-implement
-    return 11;
+    const rank = card.slice(0, -1);
+    if (rank === "A") return 11;
+    if (rank === "K" || rank === "Q" || rank === "J") return 10;
+    const num = Number(rank);
+    if (num >= 2 && num <= 10) return num;
+    return "Invalid card rank.";
 }
+
 module.exports = getCardValue;
