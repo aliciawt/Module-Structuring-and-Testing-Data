4fc50f2ad2ce221e05284959838af98aa2b391e8
aliciawt <aliciawynonatjahjadi@gmail.com>
2026-01-08 21:32:50 +0100

    Answer to Mandatory Practice - get ordinal number

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -4,10 +4,42 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // continue testing and implementing getOrdinalNumber for additional cases
 // Write your tests using Jest - remember to run your tests often for continual feedback
 
-// Case 1: Identify the ordinal number for 1
-// When the number is 1,
-// Then the function should return "1st"
+// Case 1: Identify the ordinal number for 11, 12, 13
+// When the number is 11, 12, 13
+// Then the function should return "11th", "12th", "13th"
 
-test("should return '1st' for 1", () => {
-    expect(getOrdinalNumber(1)).toEqual("1st");
+test("should return '-th' for 11, 12, 13", () => {
+    expect(getOrdinalNumber("11")).toEqual("11th");
+    });
+
+// Case 2: Identify the ordinal number numbers ending in 1
+// When the number is 1, 21, 31, etc.
+// Then the function should return "1st", "21st", "31st"
+
+test("should return '-st' for numbers ending in 1", () => {
+    expect(getOrdinalNumber("1")).toEqual("1st");
+    });
+
+// Case 3: Identify the ordinal number numbers ending in 2
+// When the number is 2, 22, 32, etc.
+// Then the function should return "2nd", "22nd", "32nd"
+
+test("should return '-nd' for numbers ending in 2", () => {
+    expect(getOrdinalNumber("2")).toEqual("2nd");
+    });
+
+// Case 4: Identify the ordinal number numbers ending in 3
+// When the number is 3, 23, 33, etc.
+// Then the function should return "3rd", "23rd", "33rd"
+
+test("should return '-rd' for numbers ending in 3", () => {
+    expect(getOrdinalNumber("3")).toEqual("3rd");
+    });
+
+// Case 5: Identify the ordinal number for other numbers not ending in 1, 2, 3
+// When the number does not end in 1, 2, 3
+// Then the function should return "num+th"
+
+test("should return '-th' for 11, 12, 13", () => {
+    expect(getOrdinalNumber("15")).toEqual("15th");
     });
