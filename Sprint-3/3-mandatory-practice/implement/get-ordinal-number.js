bc3b8784f3d4411f1f3b463e9d0b921d87cf0e75
aliciawt <aliciawynonatjahjadi@gmail.com>
2026-01-08 21:15:22 +0100

    Fixing function in Mandatory Practice - get ordinal number

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,9 @@
 function getOrdinalNumber(num) {
- return "1st";
+ if ((num === "11") || (num === "12") || (num === "13")) return `${num}th`;
+ if (num.slice(-1) === "1") return `${num}st`;
+ if (num.slice(-1) === "2") return `${num}nd`;
+ if (num.slice(-1) === "3") return `${num}rd`;
+ else return `${num}th`;
 }
 
 module.exports = getOrdinalNumber;
